3d85d3d41fca668f24e84b586b0984ac1a14469d
Stefan Lüders <ste.lueders@arcor.de>
2020-08-10 11:26:04 +0200

    Rewrite ready for testing

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ from chips.optimizer import sample
 
 out = sample(
     cloudy_input="cloudy.in",
-    cloudy_source_path="~/cloudy_optimizer/source",
+    cloudy_source_path="cloudy/source", #"~/cloudy_optimizer/source",
     output_folder="output",
     param_space={
         "T":[2, 8],
@@ -26,20 +26,20 @@ out = sample(
         "spectra/hhT6":0.1,
         "spectra/SFR":0.1
     },
-    existing_data=None,
-    initial_grid=10,
+    existing_data=None, # TODO: Testing
+    initial_grid=5,
     filename_pattern=None,
 
     dex_threshold=0.1,
     over_thresh_max_fraction=0.1,
     dex_max_allowed_diff=0.5,
     random_samples_per_iteration=30,
-    n_jobs=4,
+    n_jobs=12,
     n_partitions=10,
     max_iterations=5,
     max_storage_gb=20,
     max_time=0.1*3600,
-    plot_iterations=True
+    plot_iterations=False # TODO: Testing, Implementation
 )
 
 
